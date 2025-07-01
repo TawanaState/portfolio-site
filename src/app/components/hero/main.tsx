c23b27315b689510c6ff8994c8ffab681dfd3159
TawanaState <tawmuks@gmail.com>
2025-07-01 21:03:46 +0200

    Added the right phrases for the hero section

--- src/app/components/hero/main.tsx
+++ src/app/components/hero/main.tsx
@@ -7,20 +7,26 @@ export default function Hero() {
             <Nav />
             <div className="text-center place-self-center -translate-y-6 flex flex-col gap-1">
                 <p className="text-center">On any given day I'm</p>
+
                 <h1 className="font-secondary text-[3.2rem] leading-[3rem] h-[30vh] max-w-[90vw] md:text-[5.5rem] md:leading-[5.2rem] text-center md:max-w-[60vw] md:h-[12rem] text-wrap flex flex-row items-center justify-center overflow-clip">
                     <TextScrambleComponent
                         phrases={[
-                            "Software Engineer",
-                            "Web Developer",
-                            "Designer",
-                            "Machine Learning Engineer",
-                            "Hardware Enthusiast"
+                            "Chasing new sparks",
+                            "Wiring logic into life",
+                            "Architecting what's next",
                         ]}
                         scrambleDuration={100}
                         waitDuration={5000}
                         className="text-secondary place-self-center"
                     />
                 </h1>
+                
+                <button className="bg-secondary text-background w-fit place-self-center my-5 py-2 px-4 uppercase [clip-path:polygon(85%_0,_100%_35%,_100%_100%,_0_100%,_0_0)] flex gap-2 items-center">
+                    <span>
+                        Github
+                    </span>
+                    <span className="mso !text-lg">north_east</span>
+                </button>
             </div>
 
             <div className="place-self-end overflow-hidden -translate-y-12 -rotate-2">
