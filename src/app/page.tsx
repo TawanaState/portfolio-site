9c6c42c4e1f650c9e2bf287d060a3c09e76a0b93
TawanaState <tawmuks@gmail.com>
2025-07-18 16:15:53 +0200

    Made some major changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
+import { MotionConfig } from "motion/react"
+
 import AboutSection from "./components/about/main";
 import Main from "./components/hero/main";
 import Skills from "./components/skills/main";
 import Projects from "./components/projects/main";
+import Contact from "./components/contact/main";
+import Footer from "./components/footer/main";
+
+
 export default function Home() {
   return (
     <>
-    <Main />
-    <AboutSection />
-    <Skills />
-    <Projects />
+    <MotionConfig transition={{ duration: 0.8 }}>
+      <Main />
+      <AboutSection />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </MotionConfig>
     </>
   );
 }
