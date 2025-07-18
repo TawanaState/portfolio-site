57268dff6d0fca6568b6b875048b607dd50d288f
TawanaState <tawmuks@gmail.com>
2025-07-18 16:14:27 +0200

    Finished Skills Section

--- src/app/components/skills/main.tsx
+++ src/app/components/skills/main.tsx
@@ -1,4 +1,5 @@
 import SectionTag from "@/app/ui/section.tag";
+import * as motion from "motion/react-client";
 
 const skills = [
     {
@@ -16,16 +17,33 @@ const skills = [
 ]
 
 export default function Skills(){
+    const listVariants = {
+    hidden: {}, // no parent animation
+    visible: {
+        transition: {
+        staggerChildren: 0.2,      // 0.2s between items
+        when: "beforeChildren",    // start staggering before child animations complete
+        }
+    }
+    };
+
+    const itemVariants = {
+    hidden: { opacity: 0, outerWidth: 0 },
+    visible: {
+        opacity: 1,
+        transition: { duration: 0.3 }
+    }
+    };
 
     return(
-        <section className="flex flex-col md:grid md:grid-cols-[5fr_2fr] md:gap-6 px-mobile md:px-base py-6 pb-12">
+        <section id="skills" className="flex flex-col md:grid md:grid-cols-[5fr_2fr] md:gap-6 px-mobile md:px-base py-12 pb-12">
             <div className="flex flex-col gap-2 items-start">
                 <SectionTag>Skills</SectionTag>
                 <h2>
                     What's In My Toolkit?
                 </h2>
                 <p>
-                    It all begins with a spark of curiosity—that 'what if?' moment that pulls me into a new technology or a complex problem. The real joy then comes from the process of building, shaping, and wiring that idea into something tangible and useful that I can share with others.
+                    So, what am I usually cooking with? Think a mix of solid software engineering principles, a sprinkle of machine learning magic, and a good dose of hands-on hardware know-how. I'm always eager to pick up new tools, but these are the trusty companions that help me turn concepts into reality and tackle exciting challenges.
                 </p>
             </div>
 
@@ -35,13 +53,16 @@ export default function Skills(){
                     <div className="skill-card-clip">
                     </div>
                     <div className="skill-card-content">
-                        <img className="-translate-y-2 -translate-x-0.5" src="/icons/code.svg" alt="Code Icon" />
+                        <motion.img initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ amount: 1 }} className="-translate-y-2 -translate-x-0.5" src="/icons/code.svg" alt="Code Icon" />
                         <div className="py-2 px-4 ">
-                            <ul className="items-end justify-end">
+                            <motion.ul variants={listVariants}
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: false, amount: 0.3 }} className="items-end justify-end">
                                 {skills[0].tags.map(tag => (
-                                    <li key={tag + "--skill"}>{"[" + tag + "]"}</li>
+                                    <motion.li variants={itemVariants} key={tag + "--skill"}>{"[" + tag + "]"}</motion.li>
                                 ))}
-                            </ul>
+                            </motion.ul>
                             <h3 className="text-right">{skills[0].name}</h3>
                         </div>
                     </div>
@@ -52,15 +73,18 @@ export default function Skills(){
                     </div>
                     <div className="skill-card-content">
                         <div className="py-2 px-4">
-                            <ul className="items-start justify-start pt-3">
+                            <motion.ul variants={listVariants}
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: false, amount: 0.3 }} className="items-start justify-start pt-3">
                                 <li className="px-2">{""}</li>
                                 {skills[1].tags.map(tag => (
-                                    <li key={tag + "--skill"}>{"[" + tag + "]"}</li>
+                                    <motion.li variants={itemVariants} key={tag + "--skill"}>{"[" + tag + "]"}</motion.li>
                                 ))}
-                            </ul>
+                            </motion.ul>
                             <h3 className="text-left line-clamp-1">{skills[1].name}</h3>
                         </div>
-                        <img className="-translate-y-2 -translate-x-0.5" src="/icons/ml.svg" alt="ML Icon" />
+                        <motion.img initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ amount: 1 }} className="-translate-y-2 -translate-x-0.5" src="/icons/ml.svg" alt="ML Icon" />
 
                     </div>
                 </div>
@@ -70,13 +94,17 @@ export default function Skills(){
                     <div className="skill-card-clip">
                     </div>
                     <div className="skill-card-content">
-                        <img className="-translate-y-2 -translate-x-0.5" src="/icons/hardware.svg" alt="Hardware Icon" />
+                        <motion.img initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ amount: 1 }} className="-translate-y-2 -translate-x-0.5" src="/icons/hardware.svg" alt="Hardware Icon" />
+
                         <div className="py-2 px-4">
-                            <ul className="items-end justify-end *:first:pr-8 *:nth-[2]:pr-3">
+                            <motion.ul variants={listVariants}
+                                initial="hidden"
+                                whileInView="visible"
+                                viewport={{ once: false, amount: 0.3 }} className="items-end justify-end *:first:pr-8 *:nth-[2]:pr-3">
                                 {skills[2].tags.map(tag => (
-                                    <li key={tag + "--skill"}>{"[" + tag + "]"}</li>
+                                    <motion.li variants={itemVariants} key={tag + "--skill"}>{"[" + tag + "]"}</motion.li>
                                 ))}
-                            </ul>
+                            </motion.ul>
                             <h3 className="text-right">{skills[2].name}</h3>
                         </div>
                     </div>
